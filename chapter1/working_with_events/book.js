d2bb989ee3508057a0db766f36b9c031cb89cab7
tle <toan.lee@outlook.com>
2016-03-15 15:29:06 +0700

    adding events sample

---
+++ chapter1/working_with_events/book.js
@@ -0,0 +1,23 @@
+/**
+ * Created by tle on 3/15/2016.
+ */
+
+// book.js
+var util = require("util");
+var events = require("events");
+var Class = function() { };
+util.inherits(Class, events.EventEmitter);
+Class.prototype.ratePoints = 0;
+Class.prototype.rate = function(points) {
+ratePoints = points;
+this.emit('rated');
+};
+Class.prototype.getPoints = function() {
+return ratePoints;
+}
+module.exports = Class;
+
+
+
+
+
