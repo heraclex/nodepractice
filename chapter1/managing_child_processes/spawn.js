5897c9812de8f1d5b8f3c2bf7c83aac08a30a5b9
tle <toan.lee@outlook.com>
2016-03-15 15:20:51 +0700

    changing some files

---
+++ chapter1/managing_child_processes/spawn.js
@@ -0,0 +1,14 @@
+/**
+ * Created by tle on 3/15/2016.
+ */
+var spawn = require('child_process').spawn;
+var command = spawn('git', ['push', 'origin', 'master']);
+command.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+});
+command.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+});
+command.on('close', function (code) {
+    console.log('child process exited with code ' + code);
+});
